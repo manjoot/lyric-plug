5a52271cc18b6fe5ab9002dad0df0ca61d676392
Manjoot <manjoot@hotmail.com>
2020-04-06 15:37:37 +0100

    Adding gap between search and hotsongs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './assets/images/Lyric Plug-logo-black.png'
 import whitelogo from './assets/images/Lyric Plug-logo-white.png'
 import Search from './components/Search/index'
 import TopSongs from './components/TopSongs/index'
-import { Layout, Menu, Row, Col } from 'antd';
+import { Layout, Menu, Row, Col, Divider } from 'antd';
 import './App.css';
 
 
@@ -16,38 +16,32 @@ function App() {
       {/* Layout Declaration */}
       <Layout className="layout">
       <img class="center" src={logo} alt='logo'/>
-      <Row>
-        <Col span={6}>
-          
-        </Col>
-        <Col span={18}>
-          {/* Header and Navigation Menu */}
 
-          {/* <Header>
-
-            <div className="logo" />
-            {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1">Home</Menu.Item>
-            </Menu> */}
-          {/* </Header> */}
-
-        </Col>
-      </Row>
       {/* Content Section */}
-      <Content style={{ padding: '0 50px' }}>
-          <div>
-            <br />
-            <h1 style={{textAlign: "center"}}>Welcome! Type in your artist and song below!</h1>
-            <br />
-            <Search />
-            <div className="site-layout-content">
-              <TopSongs />
-            </div>
-            
-          </div>
-          </Content>
-          {/* Footer */}
-          <Footer style={{ textAlign: 'center' }}>Manjoot Narwal ©2020</Footer>
+        <Content style={{ padding: '0 50px' }}>
+          <Row justify="space-between">
+            {/* Search Lyrics */}
+            <Col span={11}>
+              <div className="site-layout-content">
+                <h1 style={{textAlign: "center"}}>Welcome! Type in your artist and song below!</h1>
+                <br />
+                <Search />
+              </div>
+              
+            </Col>
+
+            {/* Hot Songs */}
+            <Col span={11}>
+              <div className="site-layout-content">
+                <TopSongs />
+              </div>
+            </Col>
+          </Row>
+
+        </Content>
+        
+        {/* Footer */}
+        <Footer style={{ textAlign: 'center' }}>Manjoot Narwal ©2020</Footer>
         
       </Layout>
     </div>
