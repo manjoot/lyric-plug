124f0855a5b2cea04cba9d38b0cef27842dfc59b
Manjoot <manjoot@hotmail.com>
2020-04-06 15:43:58 +0100

    Removing clutter from HotSongs (still only initialised

--- src/components/HotSongs/index.js
+++ src/components/HotSongs/index.js
@@ -9,13 +9,9 @@ const { Meta } = Card;
 function HotSongs() {
 
     return (
-        <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
+      <div>
+        HotSongs
+      </div>
     )
 }
 
