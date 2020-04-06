446d4bf15ae026cadf37bba76970ba1c1409de89
Manjoot <manjoot@hotmail.com>
2020-04-06 12:28:13 +0100

    Removing nav clutter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import wave from './wave.svg';
 import logo from './Lyric Plug-logo-black.png'
+import whitelogo from './Lyric Plug-logo-white.png'
 import Search from './components/Search/index'
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Row, Col } from 'antd';
 import './App.css';
 
 
@@ -11,31 +12,39 @@ const { Header, Footer, Content } = Layout;
 function App() {
   return (
     <div>
+      
       {/* Layout Declaration */}
       <Layout className="layout">
+      <img class="center" src={logo} alt='logo'/>
+      <Row>
+        <Col span={6}>
+          
+        </Col>
+        <Col span={18}>
+          {/* Header and Navigation Menu */}
 
-        {/* Header and Navigation Menu */}
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">About</Menu.Item>
-          </Menu>
-        </Header>
+          {/* <Header>
 
-        {/* Content Section */}
-        <Content style={{ padding: '0 50px' }}>
+            <div className="logo" />
+            {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">Home</Menu.Item>
+            </Menu> */}
+          {/* </Header> */}
+
+        </Col>
+      </Row>
+      {/* Content Section */}
+      <Content style={{ padding: '0 50px' }}>
           <div className="site-layout-content">
-            <img height="125em" class="center" src={logo} alt='logo'/>
             <br />
             <h1 style={{textAlign: "center"}}>Welcome! Type in your artist and song below!</h1>
             <br />
             <Search />
           </div>
-        </Content>
-
-        {/* Footer */}
-        <Footer style={{ textAlign: 'center' }}>Manjoot Narwal ©2020</Footer>
+          </Content>
+          {/* Footer */}
+          <Footer style={{ textAlign: 'center' }}>Manjoot Narwal ©2020</Footer>
+        
       </Layout>
     </div>
   );
