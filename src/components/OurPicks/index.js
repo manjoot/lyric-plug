229e82b62fa37318c56dfe587337df522ab14ce0
Manjoot <manjoot@hotmail.com>
2020-04-06 15:46:23 +0100

    Initialising OurPicks section as well as tidy up hotsongs

---
+++ src/components/OurPicks/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import axios from 'axios';
+import { Card } from 'antd';
+
+//Antd declaration to support more flexible content
+const { Meta } = Card;
+
+function HotSongs() {
+
+    return (
+      <div>
+        OurPicks
+      </div>
+    )
+}
+
+export default OurPicks;
