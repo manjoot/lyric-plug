327499ac4f58c2f43b09b3869cd762d6c4e13914
Manjoot <manjoot@hotmail.com>
2020-04-07 19:04:12 +0100

    Cleaning up menu item styles

--- src/containers/Navigation/index.js
+++ src/containers/Navigation/index.js
@@ -9,8 +9,23 @@ function Navigation() {
           <nav class="main-navigation">
             <ul>
               <li style={{color: 'black', fontSize: '2em'}}>Lyric Plug</li>
-              <li><Link to="/" style={{ color: '#565555', textDecoration: 'inherit' }}>home</Link></li>
-              <li><Link to="search" style={{ color: '#565555', textDecoration: 'inherit' }}>search</Link></li>
+              <li>
+                <Link 
+                className="menu-item"
+                to="/" 
+                >
+                home
+                </Link>
+              </li>
+
+              <li>
+                <Link 
+                className="menu-item"
+                to="search" 
+                >
+                    search
+                </Link>
+              </li>
              </ul>
           </nav>
         </header>
