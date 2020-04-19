b22f347b849d87a390e72f0a71102c6c1ec87bae
Manjoot <manjoot@hotmail.com>
2020-04-19 15:57:51 +0100

    Moving Charts and Search to its own page components

--- src/pages/SearchSong.js
+++ src/pages/SearchSong.js
@@ -1,2 +1,18 @@
 import React from 'react';
-import Navigation from './containers/Navigation/index';
+import Navigation from '../containers/Navigation/index';
+import '../App.css';
+
+import Search from '../components/Search/index';
+
+const SearchSong = () => {
+  return (
+    <div className="content">
+      <Navigation />
+      <div className="content-section">
+        <Search />
+      </div>
+    </div>
+  );
+};
+
+export default SearchSong;
--- src/pages/SongCharts.js
+++ src/pages/SongCharts.js
@@ -1,2 +1,19 @@
 import React from 'react';
-import Navigation from './containers/Navigation/index';
+import Navigation from '../containers/Navigation/index';
+import '../App.css';
+
+import Charts from '../components/Charts/index';
+
+const SongCharts = () => {
+  return (
+    <div className="content">
+      <Navigation />
+      <div className="content-section">
+        <h1 style={{ textAlign: 'center', marginBottom: '1em' }}>Charts</h1>
+        <Charts />
+      </div>
+    </div>
+  );
+};
+
+export default SongCharts;
