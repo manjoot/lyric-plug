5a650e0c0d96d7f05a8c67c33779ec2be0e0451f
Manjoot <manjoot@hotmail.com>
2020-04-19 15:53:37 +0100

    Moving homepage to its own component

--- src/App.js
+++ src/App.js
@@ -1,99 +1,45 @@
 import React from 'react';
-import Navigation from './containers/Navigation/index'
-import Footer from './containers/Footer/index'
-import Search from './components/Search/index'
-import OurPicks from './components/OurPicks/index'
-import Charts from './components/Charts/index'
-import Mood from './components/Mood/index'
+import Navigation from './containers/Navigation/index';
+import Footer from './containers/Footer/index';
+import Search from './components/Search/index';
+import OurPicks from './components/OurPicks/index';
+import Charts from './components/Charts/index';
+import Mood from './components/Mood/index';
 import './App.css';
 
+import Home from './pages/Home';
+
 // Styled Components Imports
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 // Reach Router Imports
-import { Router } from '@reach/router'
-
-
-const Home = () => {
-  return (
-    <div className="content">
-      <Navigation /> 
-
-      <div className="top-gap">
-      </div>
-      
-      {/* Welcome Message */}
-
-      <div className="content-section welcome">
-        <h1 style={{textAlign:"center", marginBottom: '1em'}}>
-          Welcome!
-        </h1>
-        <p>
-          Searching for the lyrics of your favorite song? <br/>
-          <br />
-          Well your search ends now! We have a cloud based library
-          of some of the biggest tunes. Have a look around!
-        </p>
-      </div>
-
-      {/* Mood */}
-
-      <div className="content-section mood">
-        <h1 style={{textAlign:"center", marginBottom: '1em'}}>
-          Mood
-        </h1>
-        <p style={{textAlign:"center", marginBottom: '1em'}}>How are you feeling today?</p>
-        <Mood />
-      </div>
-
-      {/* Reccomended for You */}
-
-      <div className="content-section">
-        <h1 style={{textAlign:"center", marginBottom: '1em'}}>
-          Reccomended for You
-        </h1>
-        <OurPicks />
-      </div>
-
-      {/* Footer */}
-      
-      
-    </div>
-    
-
-  );
-}
+import { Router } from '@reach/router';
 
 const SearchSong = () => {
   return (
     <div className="content">
-      <Navigation/>
+      <Navigation />
       <div className="content-section">
-        <Search/>
+        <Search />
       </div>
-
     </div>
   );
-}
+};
 
 const SongCharts = () => {
   return (
     <div className="content">
-          <Navigation/>
-          <div className="content-section">
-            <h1 style={{textAlign:"center", marginBottom: '1em'}}>
-            Charts
-            </h1>
-            <Charts/>
-          </div>
-
+      <Navigation />
+      <div className="content-section">
+        <h1 style={{ textAlign: 'center', marginBottom: '1em' }}>Charts</h1>
+        <Charts />
+      </div>
     </div>
   );
-}
+};
 
 function App() {
   return (
-
     <Router>
       <Home path="/" />
       <SearchSong path="search" />
---
+++ src/pages/SearchSong.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import Navigation from './containers/Navigation/index';
