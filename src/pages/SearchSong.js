427976f0970546f5eeaa4cd0bbec8f614d804163
Manjoot <manjoot@hotmail.com>
2020-04-19 17:08:04 +0100

    Adding animations to search section

--- src/pages/SearchSong.js
+++ src/pages/SearchSong.js
@@ -7,14 +7,30 @@ import Navigation from '../containers/Navigation/index';
 // Components
 import Search from '../components/Search/index';
 
+// Popmotion Pose
+import posed from 'react-pose';
+
+const PosedContentContainer = posed.div({
+  enter: { staggerChildren: 50 },
+  exit: { staggerChildren: 20, staggerDirection: -1 },
+});
+
+const PosedContentSection = posed.div({
+  enter: { y: 0, opacity: 1 },
+  exit: { y: 50, opacity: 0 },
+});
+
 const SearchSong = () => {
   return (
-    <div className="content">
-      <Navigation />
-      <div className="content-section">
-        <Search />
+    <PosedContentContainer>
+      <div className="content">
+        <div className="content-section">
+          <PosedContentSection>
+            <Search />
+          </PosedContentSection>
+        </div>
       </div>
-    </div>
+    </PosedContentContainer>
   );
 };
 
